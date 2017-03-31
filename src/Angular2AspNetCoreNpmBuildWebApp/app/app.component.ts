b056dc7f0b6d4139ea159477c63f37f21548d2f7
aagincic <aleksandar.agincic@gmail.com>
2017-03-31 09:49:34 +0200

    Initial commit

---
+++ src/Angular2AspNetCoreNpmBuildWebApp/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from "@angular/core";
+@Component({
+    selector: "Angular-Start-Application",
+    templateUrl: './app/app.component.html'
+})
+
+
+export class AppComponent {}
